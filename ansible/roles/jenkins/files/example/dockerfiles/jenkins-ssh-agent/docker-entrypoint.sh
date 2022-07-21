4c2981cf3c10042ea2cc7698c15ab2cc0effaf39
Florian Amstutz <famstutz@outlook.com>
2022-07-21 12:54:03 +0200

    ansible: roles: jenkins: fix docker-entrypoint.sh scripts

--- ansible/roles/jenkins/files/example/dockerfiles/jenkins-ssh-agent/docker-entrypoint.sh
+++ ansible/roles/jenkins/files/example/dockerfiles/jenkins-ssh-agent/docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 # sys container init:
 #
 # If no command is passed to the container, supervisord becomes init and
-# starts all its configured programs (per /etc/supervisor/conf.f/supervisord.conf).
+# starts all its configured programs (per /etc/supervisor/conf.d/supervisord.conf).
 #
 # If a command is passed to the container, it runs in the foreground;
 # supervisord runs in the background and starts all its configured
@@ -12,8 +12,8 @@ set -e
 #
 # In either case, supervisord always starts its configured programs.
 
-if [ "$#" -eq 0 ] || [ "${1#-}" != "$1" ]; then
-  exec supervisord -n "$@"
+if [ "$#" -eq 0 ]; then
+  exec supervisord -n -c /etc/supervisor/conf.d/supervisord.conf
 else
   supervisord -c /etc/supervisor/conf.d/supervisord.conf &
   exec "$@"

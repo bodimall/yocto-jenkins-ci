1e1b7726f68e627c6d11cbfead890c0450900d4e
Florian Amstutz <famstutz@outlook.com>
2022-07-31 18:35:42 +0200

    ansible: Move example_playbook.yml to yocto_ci_playbook.yml

--- vagrant-ansible.sh
+++ vagrant-ansible.sh
@@ -10,4 +10,4 @@ ansible-playbook \
   --connection=local \
   --inventory  ${SCRIPT_DIR}/ansible/inv-example \
   --extra-vars 'ansible_python_interpreter=/usr/bin/python3' \
-  ${SCRIPT_DIR}/ansible/example_playbook.yml
+  ${SCRIPT_DIR}/ansible/yocto_ci_playbook.yml
